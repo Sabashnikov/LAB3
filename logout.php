eb1a8bc94f40784f9984b8007de26cb2f81b7a6a
Sabashnikov <160218448+Sabashnikov@users.noreply.github.com>
2024-11-12 19:52:17 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    setcookie('background_color', '', time() - 3600, "/");
-    setcookie('font_color', '', time() - 3600, "/");
-    header("Location: index.php");
-    exit();
